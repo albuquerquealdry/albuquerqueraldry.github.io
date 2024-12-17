dfbc6910e8a55954403f1dd82ea5668f55e0e1aa
albuquerquealdry <albuquerquealdry@gmail.com>
2024-12-17 09:26:14 -0300

    Deploy website - based on 2aab03646e4160ca09585a0ec794e660d7a00783

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/aldry/albuquerqueraldry.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/home/aldry/albuquerqueraldry.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/aldry/albuquerqueraldry.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/aldry/albuquerqueraldry.github.io/src/css/custom.css"),
+];

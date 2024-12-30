a2696797f628357571794418b71bb3c162a80902
albuquerquealdry <albuquerquealdry@gmail.com>
2024-12-30 08:32:21 -0300

    feat(whitepaper):ajustar cores

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,12 +66,12 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/docusaurus.png',
     navbar: {
-      title: 'My Site',
+      title: '@albuquerquealdry',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/docusaurus.png',
       },
       items: [
         {
